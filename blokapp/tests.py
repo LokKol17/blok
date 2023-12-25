b6273e79cc0096e1467cc0a06424a80171560139
Jorge Barbosa <jorge2007jfsb@gmail.com>
2023-12-25 18:21:06 -0300

    Added tests

--- blokapp/tests.py
+++ blokapp/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class HomepageTestCase(TestCase):
+    def test_homepage(self):
+        client = Client()
+
+        url = reverse('index')
+
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_addPost(self):
+        client = Client()
+
+        url = reverse('store')
+
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'store.html')
